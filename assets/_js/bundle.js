b06e49e708a42062980140f1678ac535e7b4cc87
MWDelaney <michael.delaney@gmail.com>
2018-09-21 01:36:28 -0400

    Fix dates

--- assets/_js/bundle.js
+++ assets/_js/bundle.js
@@ -2,16 +2,22 @@ import $ from 'jquery';
 
 // Get today's date
 var today = new Date();
+today.setHours(0, 0, 0, 0);
+var todayCompare = today.getMonth() + today.getDate();
 
-// Get the start of decorative gourd season this year
-var start = new Date(today.getFullYear() + '-08-22');
+// Get the start of decorative gourd season this year (month starts at 0)
+var start = new Date(today.getFullYear(), 8, 22);
+start.setHours(0, 0, 0, 0);
+var startCompare = start.getMonth() + start.getDate();
 
-// Get the end of decorative gourd season this year
-var end = new Date(today.getFullYear() + '-12-21');
+// Get the end of decorative gourd season this year (month starts at 0)
+var end = new Date(today.getFullYear(), 11, 21);
+end.setHours(0, 0, 0, 0);
+var endCompare = end.getMonth() + end.getDate();
 
 // Check if today's date is after the beginning,
 //  and before the end of decorative gourd season
-if (today >= start && today <= end) {
+if (todayCompare >= startCompare && todayCompare <= endCompare) {
     // If so, remove  the "not" section
     $('.not').remove();
 } else {
