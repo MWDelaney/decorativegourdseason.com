1fc56a32fcc2f8d5ccc29342a157183e48740cfe
MWDelaney <michael.delaney@gmail.com>
2018-11-01 11:51:02 -0400

    Bundle fixes

--- assets/_js/bundle.js
+++ assets/_js/bundle.js
@@ -3,19 +3,19 @@ import $ from 'jquery';
 // Get today's date
 var today = new Date();
 today.setHours(0, 0, 0, 0);
-var todayCompare = '' + today.getMonth() + today.getDate();
+var todayCompare = today.getTime();
 console.log(todayCompare); // eslint-disable-line
 
 // Get the start of decorative gourd season this year (month starts at 0)
 var start = new Date(today.getFullYear(), 8, 22);
 start.setHours(0, 0, 0, 0);
-var startCompare = '' + start.getMonth() + start.getDate();
+var startCompare = start.getTime();
 console.log(startCompare); // eslint-disable-line
 
 // Get the end of decorative gourd season this year (month starts at 0)
 var end = new Date(today.getFullYear(), 11, 21);
 end.setHours(0, 0, 0, 0);
-var endCompare = '' + end.getMonth() + end.getDate();
+var endCompare = end.getTime();
 console.log(endCompare); // eslint-disable-line
 
 // Check if today's date is after the beginning,
@@ -23,7 +23,6 @@ console.log(endCompare); // eslint-disable-line
 if (parseInt(todayCompare) >= parseInt(startCompare)
     && parseInt(todayCompare) <= parseInt(endCompare)) {
     // If so, remove  the "not" section
-    console.log('during'); // eslint-disable-line
     $('.not').remove();
 } else {
   // If not, remove the seson section
