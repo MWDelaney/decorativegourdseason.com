cc052d79bb1183cd729be7188cfba1440e8fbee8
MWDelaney <michael.delaney@gmail.com>
2018-11-01 11:53:01 -0400

    Remove console statements

--- assets/_js/bundle.js
+++ assets/_js/bundle.js
@@ -4,19 +4,16 @@ import $ from 'jquery';
 var today = new Date();
 today.setHours(0, 0, 0, 0);
 var todayCompare = today.getTime();
-console.log(todayCompare); // eslint-disable-line
 
 // Get the start of decorative gourd season this year (month starts at 0)
 var start = new Date(today.getFullYear(), 8, 22);
 start.setHours(0, 0, 0, 0);
 var startCompare = start.getTime();
-console.log(startCompare); // eslint-disable-line
 
 // Get the end of decorative gourd season this year (month starts at 0)
 var end = new Date(today.getFullYear(), 11, 21);
 end.setHours(0, 0, 0, 0);
 var endCompare = end.getTime();
-console.log(endCompare); // eslint-disable-line
 
 // Check if today's date is after the beginning,
 //  and before the end of decorative gourd season
